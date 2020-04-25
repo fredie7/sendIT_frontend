59e2699f312629113602b33dd6365bece00bcaae
fredie <lexfredeick3@gmail.com>
2020-04-25 10:59:18 +0100

    stateful modus fix

---
+++ js/cancelledOrders.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const state = {
+    pending : document.querySelector('.pend'),
+    delivered : document.querySelector('.deliver'),
+    cancelled : document.querySelector('.cancel'),
+    items : document.querySelector('.items'),
+    token : JSON.parse(localStorage.getItem('token')),
+}
+
+class fetchData {
+    render() {
+        fetch('http://localhost:7000/api/v1/parcels', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': state.token
+    },
+})
+.then(res=> {
+    return res.json()
+})
+.then(data=> {
+    const orderData = data
+    let clientOrder = ''
+    let totalPending = 0
+    let totalDelivered = 0
+    let totalCancelled = 0
+  orderData.forEach(order=> {
+    // console.log(order)
+    switch (order.status) {
+        case 'pending':
+            totalPending += 1
+            break;
+        case 'delivered':
+            totalDelivered += 1
+            break;
+        case 'cancelled':
+            totalCancelled += 1
+        default:
+            break;
+    }
+    if (order.status === 'cancelled') {
+        const orderHtml = `
+            <div class="itemz">
+                <div class="item-desc">
+                    <p id="desc">${order.description}</p>
+                </div>
+                <div class="item-details">
+                    <p>Order <span id="status">${order.status}</span></p>
+                    <p>Created on <span id="created">${order.createdAt}</span></p>
+                </div>
+                <button class="view-order">View Order</button>
+            </div>
+        `
+        clientOrder += orderHtml
+    }
+        
+  })
+  state.items.innerHTML = clientOrder;  
+  console.log(totalPending, totalDelivered)
+  state.pending.textContent = totalPending
+  state.delivered.textContent = totalDelivered
+})
+    }
+}
+
+class sendData {
+    render() {
+        const pageDisplay = new fetchData().render()
+        return pageDisplay
+    }
+}
+
+const root = new sendData()
+root.render()
--- js/signup.js
+++ js/signup.js
@@ -1,60 +1,76 @@
 'use strict'
 
-const signupBtn = document.getElementById('signup-btn')
-const form = document.querySelector('form')
-const firstName = document.getElementById('f-name')
-const lastName = document.getElementById('l-name')
-const email = document.getElementById('email')
-const userName = document.getElementById('username')
-const password = document.getElementById('password')
-const signupTab = document.querySelector('.form-theme')
+const state = {
+    signupBtn : document.getElementById('signup-btn'),
+    form : document.querySelector('form'),
+    firstName : document.getElementById('f-name'),
+    lastName : document.getElementById('l-name'),
+    email : document.getElementById('email'),
+    userName : document.getElementById('username'),
+    password : document.getElementById('password'),
+    signupTab : document.querySelector('.form-theme'),
+}
 
-signupBtn.addEventListener('click', ()=> {
-    if (lastName.value !== '' || email.value !== '' || userName.value !== '' || password.value !== '') {
-    const signupData = {
-        name: `${firstName.value} ${lastName.value}`,
-        userName: userName.value,
-        email: email.value,
-        password: password.value,
-    }
-    firstName.value = ''
-    lastName.value = ''
-    email.value = ''
-    userName.value = ''
-    password.value = ''
-    
-    fetch("http://localhost:7000/api/v1/auth/signup", {
-        method: 'POST',
-        headers: {
-            'content-Type' : 'application/json'
-        },
-        body: JSON.stringify(signupData)
-    })
-    .then(res => {
-        if (!res.ok) {
-            //handle failure case
-                signupTab.textContent = 'user already exists'
-                signupTab.classList.add('danger')
+class fetchData {
+    render() {
+        state.signupBtn.addEventListener('click', ()=> {
+            if (state.lastName.value !== '' || state.email.value !== '' || state.userName.value !== '' || state.password.value !== '') {
+            const signupData = {
+                name: `${state.firstName.value} ${state.lastName.value}`,
+                userName: state.userName.value,
+                email: state.email.value,
+                password: state.password.value,
+            }
+            state.firstName.value = ''
+            state.lastName.value = ''
+            state.email.value = ''
+            state.userName.value = ''
+            state.password.value = ''
+            
+            fetch("http://localhost:7000/api/v1/auth/signup", {
+                method: 'POST',
+                headers: {
+                    'content-Type' : 'application/json'
+                },
+                body: JSON.stringify(signupData)
+            })
+            .then(res => {
+                if (!res.ok) {
+                    //handle failure case
+                        state.signupTab.textContent = 'user already exists'
+                        state.signupTab.classList.add('danger')
+                        setTimeout(() => {
+                            window.location.reload()
+                        }, 3000);                
+                        // throw new Error('something went wrong with your fetch')
+                        return;
+                    }
+                    // console.log(res.json())
+                    state.signupTab.textContent = 'signup successful'
+                    state.signupTab.classList.add('success')
+                    setTimeout(() => {
+                        window.location.reload()
+                    }, 3000);                
+                // handle success case
+            })
+            } else {
+                state.signupTab.textContent = 'please, fill in all fields'
+                // state.signupTab.style.color = 'red'
+                state.signupTab.classList.add('danger')
                 setTimeout(() => {
                     window.location.reload()
-                }, 3000);                
-                // throw new Error('something went wrong with your fetch')
-                return;
-            }
-            // console.log(res.json())
-            signupTab.textContent = 'signup successful'
-            signupTab.classList.add('success')
-            setTimeout(() => {
-                window.location.reload()
-            }, 3000);                
-        // handle success case
-    })
-    } else {
-        signupTab.textContent = 'please, fill in all fields'
-        // signupTab.style.color = 'red'
-        signupTab.classList.add('danger')
-        setTimeout(() => {
-            window.location.reload()
-        }, 3000);
+                }, 3000);
+        }
+        })
+    }
 }
-})
+
+class sendData {
+    render() {
+        const pageDisplay = new fetchData().render()
+        return pageDisplay
+    }
+}
+
+const root = new sendData()
+root.render()
